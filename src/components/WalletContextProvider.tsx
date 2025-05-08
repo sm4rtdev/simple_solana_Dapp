c40daa16af0479c79d8f60cb1fc21997438a5a23
sm4rtdev <elonscottdev@gmail.com>
2025-05-08 15:12:51 -0500

    wallet integration

---
+++ src/components/WalletContextProvider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { FC, ReactNode, useMemo } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
+export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const network = WalletAdapterNetwork.Devnet
+    const endpoint = 'https://api.devnet.solana.com'
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
